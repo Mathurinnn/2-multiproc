cfbdcaa56bd7062980ee3742b5b51a37e49acc4a
Mathurin <mathurin.verclytte@epfl.ch>
2023-11-06 21:24:44 +0100

    okkk

--- algorithm.c
+++ algorithm.c
@@ -13,49 +13,52 @@ SCIPER      : 346981, 346153
 
 void simulate(double *input, double *output, int threads, int length, int iterations) {
     double *temp;
-    double values[length*length/threads];
-    int is[length*length/threads];
-    int js[length*length/threads];
-    int count;
     omp_set_num_threads(threads);
 
     for (int n = 0; n < iterations; n++) {
 
-        #pragma omp parallel default(none) private(values, is, js, count) shared(iterations, input, output, length, temp, threads)
+#pragma omp parallel default(none) shared(iterations, input, output, length, temp, threads)
         {
-            count = -1;
-            for (int i = 0; i < length*length/threads +1; ++i) {
-                is[i] = -1;
-                js[i] = -1;
-                values[i] = 0;
-            }
 
-            #pragma omp for collapse(2)
+#pragma omp for
             for (int i = 1; i < length - 1; i++) {
-                for (int j = 1; j < length - 1; j++) {
+                for (int j = 1; j < length - 1; j += 2) {
+
                     if (((i == length / 2 - 1) || (i == length / 2))
                         && ((j == length / 2 - 1) || (j == length / 2)))
                         continue;
 
-                    count++;
-                    is[count] = i;
-                    js[count] = j;
-                    values[count] = (INPUT(i - 1, j - 1) + INPUT(i - 1, j) + INPUT(i - 1, j + 1) +
-                            INPUT(i, j - 1) + INPUT(i, j) + INPUT(i, j + 1) +
-                            INPUT(i + 1, j - 1) + INPUT(i + 1, j) + INPUT(i + 1, j + 1)) / 9;
+                    double c1 = INPUT(i - 1, j - 1);
+                    double c2 = INPUT(i - 1, j);
+                    double c3 = INPUT(i - 1, j + 1);
+                    double c4 = INPUT(i, j - 1);
+                    double c5 = INPUT(i, j);
+                    double c6 = INPUT(i, j + 1);
+                    double c7 = INPUT(i + 1, j - 1);
+                    double c8 = INPUT(i + 1, j);
+                    double c9 = INPUT(i + 1, j + 1);
+
+
+                    OUTPUT(i,j) = (c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9) / 9;
+
+                    if (((i == length / 2 - 1) || (i == length / 2))
+                        && ((j +1 == length / 2 - 1) || (j +1 == length / 2)))
+                        continue;
+
+                    double c10 = INPUT(i - 1, j + 2);
+                    double c11 = INPUT(i, j + 2);
+                    double c12 = INPUT(i + 1, j + 2);
+
+                    OUTPUT(i,j+1) = (c2 + c3 + c10 +
+                                     c5 + c6 + c11 +
+                                     c8 + c9 + c12) / 9;
+
                 }
             }
-            
-            for (int i = 0; i < length * length/threads; ++i) {
 
-                if (is[i] == -1) break;
-
-                #pragma omp atomic
-                OUTPUT(is[i], js[i]) += values[i];
-            }
         }
-                temp = input;
-                input = output;
-                output = temp;
+        temp = input;
+        input = output;
+        output = temp;
     }
 }
